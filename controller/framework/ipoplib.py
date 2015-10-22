d1345189f0e3316a10ac41fda08b37c3d7092dd4
ssabogal <batin84@ufl.edu>
2015-10-22 19:23:23 -0400

    merge: fix generic bugs during merge process: GVPN now works

--- controller/framework/ipoplib.py
+++ controller/framework/ipoplib.py
@@ -42,6 +42,50 @@ CONFIG = {
         "stat_report": False,
         "stat_server": "metrics.ipop-project.org",
         "stat_server_port": 5000
+    },
+    "TincanListener": {
+        "buf_size": 65507,
+        "socket_read_wait_time": 15,
+        "joinEnabled": True
+    },
+    "Logger": {
+        "controller_logging": "INFO",
+        "joinEnabled": True
+    },
+    "TincanDispatcher": {
+        "joinEnabled": True
+    },
+    "BaseTopologyManager": {
+        "ip4": "192.168.5.1",
+        "link_trimmer_wait_time": 30,
+        "on-demand_connection": False,
+        "on-demand_inactive_timeout": 600,
+        "multihop": False,
+        "sec": True,
+        "timer_interval": 15,
+        "joinEnabled": True
+    },
+    "LinkManager": {
+        "joinEnabled": True
+    },
+    "TincanSender": {
+        "stun": ["stun.l.google.com:19302", "stun1.l.google.com:19302",
+                 "stun2.l.google.com:19302", "stun3.l.google.com:19302",
+                 "stun4.l.google.com:19302"],
+        "turn": [],
+        "ip6_prefix": "fd50:0dbc:41f2:4a3c",
+        "switchmode": 0,
+        "localhost": "127.0.0.1",
+        "svpn_port": 5800,
+        "localhost6": "::1",
+        "joinEnabled": True
+    },
+    "CentralVisualizer": {
+        "central_visualizer": False,
+        "central_visualizer_addr": "",
+        "central_visualizer_port": 0,
+        "joinEnabled": True,
+        "dependencies": ["Logger"]
     }
 }
 
