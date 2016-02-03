54a851e86ec36267a271050e607b769255ee74d9
saumitra <saumitraaditya@gmail.com>
2016-02-03 12:21:04 -0800

    serialized access to shared state-all access to Binding,state should be protected, change in state should be atomic

--- controller/modules/NetworkLeaderElector.py
+++ controller/modules/NetworkLeaderElector.py
@@ -83,7 +83,7 @@ class NetworkLeaderElector(ControllerModule):
     def timer_method(self):
         # Send out LSA Binding's every iteration 
         # May be a issue in large all to all network.
-        payload = self.Binding
+        payload = self.checkState()[1]
         payload[2] = self.uid # change source to self.
         msg = {
                 "msg_type": "LSA",
@@ -113,6 +113,7 @@ class NetworkLeaderElector(ControllerModule):
                 
     def changeState(self,toState,msg):
         ####   LOCK HERE ######
+        self.CFxHandle.syncLock.acquire()
             if (toState == "REMOTE"):
                 # clear membership list
                 del self.members[:]
@@ -137,11 +138,14 @@ class NetworkLeaderElector(ControllerModule):
                 self.Binding = {"priority":self.priority,"leader":self.uid,"source":self.uid}
                 self.LCM_State = "PENDING"
         #### UNLOCK HERE ######
+        self.CFxHandle.syncLock.release()
         
     def checkState(self):
         #### LOCK ####
+        self.CFxHandle.syncLock.acquire()
         return [self.LCM_State,self.Binding]
         #### UNLOCK ####
+        self.CFxHandle.syncLock.release()
         
     def send_PingToLeader(self,stack = []):
         msg = {
@@ -156,7 +160,7 @@ class NetworkLeaderElector(ControllerModule):
         # link might exist.
         # The ICC message has to be sent to the 
         # source in binding.
-        nexthop_uid = self.Binding.get("source")
+        nexthop_uid = self.checkState[1].get("source")
         if (self.peers[nexthop_uid] == "online"):
             self.send_msg_icc(nexthop_uid, msg)
             
@@ -171,14 +175,15 @@ class NetworkLeaderElector(ControllerModule):
             self.send_msg_icc(nexthop_uid, msg)
             
     def send_Join(self,stack=[]):
+        Binding = self.checkState[1]
         msg = {
                 "msg_type": "JOIN",
                 "src_uid": self.uid,
-                "target_uid" : self.Binding.get("leader"),
+                "target_uid" : Binding.get("leader"),
                 "stack": stack.append(self.uid),
                 "TTL": 0
                 }
-        nexthop_uid = self.Binding.get("source")
+        nexthop_uid = Binding.get("source")
         if (self.peers[nexthop_uid] == "online"):
             self.send_msg_icc(nexthop_uid, msg)
     
@@ -193,36 +198,38 @@ class NetworkLeaderElector(ControllerModule):
             self.send_msg_icc(nexthop_uid, msg)
             
     def send_Leave(self,stack=[]):
+        Binding = self.checkState()[1]
         msg = {
                 "msg_type": "LEAVE",
                 "src_uid": self.uid,
-                "target_uid" : self.Binding.get("leader"),
+                "target_uid" : Binding.get("leader"),
                 "stack": stack.append(self.uid),
                 "TTL": 0
                 }
-        nexthop_uid = self.Binding.get("source")
+        nexthop_uid = Binding.get("source")
         if (self.peers[nexthop_uid] == "online"):
             self.send_msg_icc(nexthop_uid, msg)
     # This method forwards messages using ICC 
     # It is possible that there be no direct
     # link between the node and leader
     def forward_msg(self,msg):
+        Binding = self.checkState()[1]
         if (msg.get("msg_type") == "PING_LEADER"):
             msg["stack"] = msg["stack"].append(self.uid)
             msg["TTL"] = msg["TTL"] + 1
-            nexthop_uid = self.Binding.get("source")
+            nexthop_uid = Binding.get("source")
         elif (msg.get("msg_type") == "PING_LEADER_ACK"):
             nexthop_uid = msg["stack"].pop()
         elif (msg.get("msg_type") == "JOIN"):
             msg["stack"] = msg["stack"].append(self.uid)
             msg["TTL"] = msg["TTL"] + 1
-            nexthop_uid = self.Binding.get("source")
+            nexthop_uid = Binding.get("source")
         elif (msg.get("msg_type") == "JOIN_ACK"):
             nexthop_uid = msg["stack"].pop()
         elif (msg.get("msg_type") == "LEAVE"):
             msg["stack"] = msg["stack"].append(self.uid)
             msg["TTL"] = msg["TTL"] + 1
-            nexthop_uid = self.Binding.get("source")
+            nexthop_uid = Binding.get("source")
         # Whatever be the type of msg above we 
         # select next_hop uid and the message
         # next we just send the msg using ICC 
@@ -273,8 +280,10 @@ class NetworkLeaderElector(ControllerModule):
             #    6. LEAVE                                                     #                                  
             #    7. LEAVE_ACK                                                 #                                      
             ###################################################################
+            curr_state = self.checkState()[0]
+            Binding = self.checkState()[1]
             if (msg_type == "LSA"):
-                if (self.checkState()[0] == "EMPTY"):
+                if (curr_state == "EMPTY"):
                     if (msg.get("priority") > self.priority):
                         # set leader ping timeout
                         # set retry value
@@ -285,7 +294,7 @@ class NetworkLeaderElector(ControllerModule):
                         self.changeState("REMOTE",msg)
                     else:
                         self.changeState("LOCAL",msg)
-                elif (self.checkState()[0] == "LOCAL"):
+                elif (curr_state == "LOCAL"):
                     if (msg.get("priority") > self.priority):
                         # set leader ping timeout
                         # set retry value
@@ -297,12 +306,12 @@ class NetworkLeaderElector(ControllerModule):
                     else:
                         # Do nothing-stay in local state
                         pass
-                elif (self.checkState()[0] == "REMOTE"):
-                    if (msg.get("priority") > self.Binding.get("priority")):
+                elif (curr_state == "REMOTE"):
+                    if (msg.get("priority") > Binding.get("priority")):
                         # set leader ping timeout
                         # set retry value
                         # start pinging leader
-                        # send LEAVE to old leader---------------------------------------------
+                        # send LEAVE to old leader
                         self.send_Leave()
                         # send join to new leader
                         # clear membership table
@@ -311,8 +320,8 @@ class NetworkLeaderElector(ControllerModule):
                     else:
                         # Do nothing stay in the same state.
                         pass
-                elif (self.checkState()[0] == "PENDING"):
-                    if (msg.get("priority") > self.Binding.get("priority")):
+                elif (curr_state == "PENDING"):
+                    if (msg.get("priority") > Binding.get("priority")):
                         # set leader ping timeout
                         # set retry value
                         # start pinging leader
@@ -328,13 +337,13 @@ class NetworkLeaderElector(ControllerModule):
                 if (msg.get("target_uid") != self.uid and msg["TTL"]<10):
                     self.forward_msg(msg)
                 else:
-                    if (msg.get("target_uid")== self.uid and self.checkState()[0] == "LOCAL"):
+                    if (msg.get("target_uid")== self.uid and curr_state == "LOCAL"):
                         # Return a PING_LEADER_ACK message
                         self.send_PingLeaderAck(stack)
             elif (msg_type == "JOIN"):
                 if (msg.get("target_uid") != self.uid and msg["TTL"]<10):
                     self.forward_msg(msg)
-                elif (msg.get("target_uid")== self.uid and self.checkState()[0] == "LOCAL"):
+                elif (msg.get("target_uid")== self.uid and curr_state == "LOCAL"):
                     # Add member to member's list
                     self.members.append(msg.get("src_uid"))
                     # return JOIN_ACK message
@@ -342,31 +351,31 @@ class NetworkLeaderElector(ControllerModule):
             elif (msg_type == "LEAVE"):
                 if (msg.get("target_uid") != self.uid and msg["TTL"]<10):
                     self.forward_msg(msg)
-                elif (msg.get("target_uid")== self.uid and self.checkState()[0] == "LOCAL"):
+                elif (msg.get("target_uid")== self.uid and curr_state == "LOCAL"):
                     # remove member from member's list
                     self.members.remove(msg.get("src_uid"))
                    
-            elif (msg_type == "PING_LEADER_ACK" and self.checkState()[0] == "REMOTE"):
+            elif (msg_type == "PING_LEADER_ACK" and curr_state == "REMOTE"):
                 stack = msg.get("stack)
                 if (len(stack)==0):
                     # I am the intended recipient
                     # check if response if from current leader
                     # send ping message again
-                    if (msg.get("src_uid") == self.Binding.get("leader")):
+                    if (msg.get("src_uid") == Binding.get("leader")):
                         self.send_PingToLeader()
                         self.pingTimerStart = int(time.time())
                         self.ping_retry_counter = 0
                 else:
                     self.forward_msg(msg)
                 
-            elif (msg_type == "JOIN_ACK" and self.checkState()[0] == "REMOTE"):
+            elif (msg_type == "JOIN_ACK" and curr_state == "REMOTE"):
                 # check if the response is from current leader
                 stack = msg.get("stack)
                 if (len(stack)==0):
                     # I am the intended recipient
                     # check if response if from current leader
                     # send ping message again
-                    if (msg.get("src_uid") == self.Binding.get("leader")):
+                    if (msg.get("src_uid") == Binding.get("leader")):
                         self.MSM_State = "MEMBER"
                 else:
                     self.forward_msg(msg)
