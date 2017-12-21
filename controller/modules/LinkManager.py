573e7843f787ca1eeb3f3f0476f5d82dd5edf464
ken <kcratie@users.noreply.github.com>
2017-12-21 13:09:02 -0500

    Review changes for Signal module.

--- controller/modules/LinkManager.py
+++ controller/modules/LinkManager.py
@@ -56,7 +56,7 @@ class LinkManager(ControllerModule):
     # Forward cbt over XMPP
     def forward_cbt(self,interface_name,peer_uid,payload):
         cbtdata = {"uid": peer_uid, "data": payload, "interface_name":interface_name}
-        self.registerCBT(self.link_details[interface_name]["xmpp_client_code"], "FORWARD_CBT",cbtdata)
+        self.registerCBT(self.link_details[interface_name]["xmpp_client_code"], "SIG_FORWARD_CBT",cbtdata)
 
     # send message (through ICC)
     #   - uid = UID of the destination peer (a tincan link must exist)
