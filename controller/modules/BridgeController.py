6230f8a3f945563d7412a07266c63ea8c43c30b0
ken <kcratie@users.noreply.github.com>
2018-04-05 15:04:27 -0400

    Adds ipop tap to bridge

---
+++ controller/modules/BridgeController.py
@@ -0,0 +1,213 @@
+# ipop-project
+# Copyright 2016, University of Florida
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+
+from abc import ABCMeta, abstractmethod
+import threading
+from distutils import spawn
+import controller.framework.ipoplib as ipoplib
+from controller.framework.ControllerModule import ControllerModule
+
+IPEXE = spawn.find_executable("ip")
+
+class BridgeABC():
+    __metaclass__ = ABCMeta
+    def __init__(self, name, ip_addr, prefix_len):
+        self.name = name
+        self.ip_addr = ip_addr
+        self.prefix_len = prefix_len
+
+    @abstractmethod
+    def add_port(self, port_name):
+        pass
+
+    @abstractmethod
+    def del_port(self, port_name):
+        pass
+
+    @abstractmethod
+    def del_br(self, port_name):
+        pass
+
+    @property
+    @abstractmethod
+    def brctlexe(self,):
+        pass
+
+class OvsBridge(BridgeABC):
+    brctlexe = spawn.find_executable("ovs-ctl")
+    def __init__(self, name, ip_addr, prefix_len):
+        """ Initialize a bridge object. """
+        super(OvsBridge, self).__init__(name, ip_addr, prefix_len)
+        ipoplib.runshell_su([OvsBridge.brctlexe, "--may-exist", "add-br", self.name])
+
+        net = "{0}/{1}".format(ip_addr, prefix_len)
+        ipoplib.runshell_su([IPEXE, "addr", "add", net, "dev", self.name,])
+        ipoplib.runshell_su([IPEXE, "link", "set", "dev", self.name, "up"])
+
+    def del_br(self, port_name):
+        ipoplib.runshell_su([OvsBridge.brctlexe, "--if-exists", "del-br", self.name])
+
+    def add_port(self, port_name):
+        ipoplib.runshell_su([OvsBridge.brctlexe, "--may-exist", "add-port", self.name, port_name])
+
+    def del_port(self, port_name):
+        ipoplib.runshell_su([OvsBridge.brctlexe, "--if-exists", "del-port", self.name, port_name])
+
+class LinuxBridge(BridgeABC):
+    brctlexe = spawn.find_executable("brctl")
+    #IPEXE = spawn.find_executable("ip")
+
+    def __init__(self, name, ip_addr, prefix_len):
+        """ Initialize a bridge object. """
+        super(LinuxBridge, self).__init__(name, ip_addr, prefix_len)
+
+
+        p = ipoplib.runshell_su([LinuxBridge.brctlexe, 'show'])
+        wlist = map(str.split, p.stdout.decode("utf-8").splitlines()[1:])
+        brwlist = filter(lambda x: len(x) != 1, wlist)
+        brlist = map(lambda x: x[0], brwlist)
+        for br in brlist:
+            print(br)
+            if br == name:
+                print("deleting {}".format(br))
+                self.del_br()
+
+        ipoplib.runshell_su([LinuxBridge.brctlexe, "addbr", self.name])
+        net = "{0}/{1}".format(ip_addr, prefix_len)
+        ipoplib.runshell_su([IPEXE, "addr", "add", net, "dev", name,])
+        self.stp(True)
+        ipoplib.runshell_su([IPEXE, "link", "set", "dev", name, "up"])
+
+    def __str__(self):
+        """ Return a string of the bridge name. """
+        return self.name
+
+    def __repr__(self):
+        """ Return a representaion of a bridge object. """
+        return "<Bridge: %s>" % self.name
+
+    def del_br(self):
+        """ Set the device down and delete the bridge. """
+        ipoplib.runshell_su([IPEXE, "link", "set", "dev", self.name, "down"])
+        ipoplib.runshell_su([LinuxBridge.brctlexe, "delbr", self.name])
+
+    def add_port(self, port):
+        ipoplib.runshell_su([LinuxBridge.brctlexe, "addif", self.name, port])
+
+    def del_port(self, port):
+        ipoplib.runshell_su([LinuxBridge.brctlexe, "delif", self.name, port])
+
+    def stp(self, val=True):
+        """ Turn STP protocol on/off. """
+        if val: state = "on" 
+        else: state = "off"
+        ipoplib.runshell_su([LinuxBridge.brctlexe, "stp", self.name, state])
+
+    def set_bridge_prio(self, prio):
+        """ Set bridge priority value. """
+        ipoplib.runshell_su([LinuxBridge.brctlexe, "setbridgeprio", self.name, str(prio)])
+
+    def set_path_cost(self, port, cost):
+        """ Set port path cost value for STP protocol. """
+        ipoplib.runshell_su([LinuxBridge.brctlexe, "setpathcost", self.name, port, str(cost)])
+
+    def set_port_prio(self, port, prio):
+        """ Set port priority value. """
+        ipoplib.runshell_su([LinuxBridge.brctlexe, "setportprio", self.name, port, str(prio)])
+
+class BridgeController(ControllerModule):
+    def __init__(self, cfx_handle, module_config, module_name):
+        super(BridgeController, self).__init__(cfx_handle, module_config,
+            module_name)
+        self._overlays = dict()
+        self._lock = threading.Lock()
+
+    def initialize(self):
+        for olid in self._cm_config["Overlays"]:
+            if self._cm_config["Overlays"][olid]["Type"] == "LXBR":
+                self._overlays[olid] = LinuxBridge(self._cm_config["Overlays"][olid]["BridgeName"],
+                                                   self._cm_config["Overlays"][olid]["IP4"],
+                                                   self._cm_config["Overlays"][olid]["PrefixLen"])
+            elif self._cm_config["Overlays"][olid]["Type"] == "OVS":
+                self._overlays[olid] = OvsBridge(self._cm_config["Overlays"][olid]["BridgeName"],
+                                                 self._cm_config["Overlays"][olid]["IP4"],
+                                                 self._cm_config["Overlays"][olid]["PrefixLen"])
+
+        self._cfx_handle.start_subscription("LinkManager", "LNK_DATA_UPDATES")
+        self.register_cbt("Logger", "LOG_INFO", "Module Loaded")
+
+    def req_handler_add_port(self, cbt):
+        pass
+
+    def req_handler_del_port(self, cbt):
+        pass
+
+    def req_handler_manage_bridge(self, cbt):
+        try:
+            olid = cbt.request.params["OverlayId"]
+            port_name = cbt.request.params["TapName"]
+            br = self._overlays[olid]
+            if cbt.request.params["UpdateType"] == "ADDED":
+                br.add_port(port_name)
+                self.register_cbt("Logger", "LOG_INFO","Port {0} added to bridge {1}"
+                                  .format(port_name, str(br)))
+            elif cbt.request.params["UpdateType"] == "REMOVED":
+                br.del_port(port_name)
+                self.register_cbt("Logger", "LOG_INFO","Port {0} removed from bridge {1}"
+                                  .format(port_name, str(br)))
+        except RuntimeError as err:
+            self.register_cbt("Logger", "LOG_WARNING", str(err))
+        cbt.set_response(None, True)
+        self.complete_cbt(cbt)
+
+    def resp_handler_(self, cbt):
+        pass
+
+    def process_cbt(self, cbt):
+        if cbt.op_type == "Request":
+            if cbt.request.action == "BRG_ADD_PORT":
+                self.req_handler_add_port(cbt)
+            if cbt.request.action == "BRG_DEL_PORT":
+                self.req_handler_del_port(cbt)
+            if cbt.request.action == "LNK_DATA_UPDATES":
+                self.req_handler_manage_bridge(cbt)
+            else:
+                self.req_handler_default(cbt)
+        elif cbt.op_type == "Response":
+            if cbt.request.action == "TOP_QUERY_PEER_IDS":
+                self.resp_handler_query_peers(cbt)
+            else:
+                parent_cbt = self.get_parent_cbt(cbt)
+                cbt_data = cbt.response.data
+                cbt_status = cbt.response.status
+                self.free_cbt(cbt)
+                if (parent_cbt is not None and parent_cbt.child_count == 1):
+                    parent_cbt.set_response(cbt_data, cbt_status)
+                    self.complete_cbt(parent_cbt)
+
+
+    def timer_method(self):
+        pass
+
+    def terminate(self):
+        for olid in self._overlays:
+            self._overlays[olid].del_br()
